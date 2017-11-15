ac8f7cc69b63a313b00795b91b7c54d5e81731cc
stalwar7 <stalwar@cpp.edu>
2017-11-14 18:43:33 -0800

    edits

--- SortedLinkedData.java
+++ SortedLinkedData.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class SortedLinkedData<K, V> implements DictionaryInterface<K, V> {
 
@@ -12,18 +14,18 @@ public class SortedLinkedData<K, V> implements DictionaryInterface<K, V> {
 
 	public V add(K key, V value) {
 		boolean result = false;
-		while (head.next != null) {			//replaces
+		while (head.next != null) { // replaces
 			if (head.key == key) {
 				result = true;
 				return head.value;
 			}
 			head = head.next;
 		}
-		if (head == null) {					//first entry
+		if (head == null) { // first entry
 			head.key = key;
 			head.value = value;
 		}
-		if (!result) {						//add 
+		if (!result) { // add
 			Node node = new Node();
 			head.next = node;
 			node.key = key;
@@ -36,13 +38,11 @@ public class SortedLinkedData<K, V> implements DictionaryInterface<K, V> {
 
 	public V remove(K key) {
 		V temp = null;
-		while(head.next != null)
-		{
-			if(head.key == key)
-			{
+		while (head.next != null) {
+			if (head.key == key) {
 				temp = head.value;
 				head.key = null;
-			}	
+			}
 			head = head.next;
 		}
 		return temp;
@@ -67,13 +67,21 @@ public class SortedLinkedData<K, V> implements DictionaryInterface<K, V> {
 	}
 
 	public Iterator<K> getKeyIterator() {
-		// TODO Auto-generated method stub
-		return null;
+		List<K> iter = new ArrayList<K>();
+		while (head.next != null) {
+			iter.add(head.key);
+		}
+
+		return iter.iterator();
 	}
 
 	public Iterator<V> getValueIterator() {
-		// TODO Auto-generated method stub
-		return null;
+		List<V> iter = new ArrayList<V>();
+		while (head.next != null) {
+			iter.add(head.value);
+		}
+
+		return iter.iterator();
 	}
 
 	public boolean isEmpty() {
@@ -85,8 +93,7 @@ public class SortedLinkedData<K, V> implements DictionaryInterface<K, V> {
 	}
 
 	public void clear() {
-		while(head.next != null)
-		{
+		while (head.next != null) {
 			head.key = null;
 			head.value = null;
 			head = head.next;
